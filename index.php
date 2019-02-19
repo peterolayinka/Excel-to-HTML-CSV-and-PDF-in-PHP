a0fcac7b1d154e5ba763afd028436a432d9009a5
Peter <peter.olayinka@britecore.com>
2019-02-19 02:21:15 +0100

    concluded project

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 Dompdf\Autoloader::register();
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
+use Dompdf\Options;
 
 /**  Define a Read Filter class implementing \PhpOffice\PhpSpreadsheet\Reader\IReadFilter  */
 class MyReadFilter implements \PhpOffice\PhpSpreadsheet\Reader\IReadFilter
@@ -36,14 +37,12 @@ class PDFHelper {
     private $worksheet;
     private $spreadsheet;
 
-    /* set value to 'private $name' property */
     public function getWorksheet($fileName){
         /**  Create an Instance of our Read Filter  **/
         $filterSubset = new MyReadFilter($this->minPageNum, $this->maxPageNum,range('A','Y'));
 
         $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
         // $reader->setLoadSheetsOnly("Sheet1");
-        /**  Tell the Reader that we want to use the Read Filter  **/
         $reader->setReadFilter($filterSubset);
         $this->spreadsheet = $reader->load($fileName);
         $this->worksheet = $this->spreadsheet->getSheet(0)->toArray(null, true, true, true);
@@ -52,66 +51,46 @@ class PDFHelper {
     }
 
     public function getHTML(){
-        // echo '<pre>';
-        // print_r($this->worksheet);
-        // echo '--------------';
-        // // print_r($spreadsheet);
-        // echo '</pre>';
-
-        //now it is created a html table with the excel file data
-        $html_tb ='<table border="1"><tr><th>'. implode('</th><th>', $this->worksheet[1]) .'</th></tr>';
+        //html table with the excel file data
+        $html_tb ='<font size="1" face="Courier New" >';
+        $html_tb .='<table border="1"><tr><th>'. implode('</th><th>', $this->worksheet[1]) .'</th></tr>';
         $nr = count($this->worksheet); //number of rows
         for($i=2; $i<=$nr; $i++){
           $html_tb .='<tr><td>'. implode('</td><td>', $this->worksheet[$i]) .'</td></tr>';
         }
-        $html_tb .='</table>';
+        $html_tb .='</table></font>';
         return $html_tb;
     }
 
+    function getCSV(){
+        // redirect output to client browser
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment;filename="myfile.csv"');
+        header('Cache-Control: max-age=0');
+
+        $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($this->spreadsheet);
+        $writer->save('php://output');
+    }
+
+    function getPDF(){
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($this->getHTML());
+
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A2', 'landscape');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser
+        $dompdf->stream();
+    }
 }
 
 
 $fileName = __DIR__ ."/GooglePlaySept2018.xlsx";
 
-$pdfHelper = new PDFHelper($fileName);
+$pdfHelper = new PDFHelper();
 $pdfHelper->getWorksheet($fileName);
-echo $pdfHelper->getHTML();
-
-
-
-
-
-
-// echo $html_tb;
-
-
-
-
-// echo '<pre>';
-// var_dump($oTrends);
-// var_dump($spreadsheet);
-// print_r($worksheet);
-// echo '--------------';
-// // print_r($spreadsheet);
-// echo '</pre>';
-// // print $spreadsheet;
-
-// $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
-// $writer->save("05featuredemo.csv");
-
-// $writer = new \PhpOffice\PhpSpreadsheet\Writer\Html($spreadsheet);
-
-// $writer->save("05featuredemo.html");
-
-// instantiate and use the dompdf class
-// $dompdf = new Dompdf();
-// $dompdf->loadHtml($html_tb);
-
-// // (Optional) Setup the paper size and orientation
-// $dompdf->setPaper('A4', 'landscape');
-
-// // Render the HTML as PDF
-// $dompdf->render();
-
-// // Output the generated PDF to Browser
-// $dompdf->stream();
+echo $pdfHelper->getCSV();
