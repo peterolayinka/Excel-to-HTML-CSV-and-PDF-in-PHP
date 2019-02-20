bb12374678856f0e1c78ce05a3aea27ee33561e9
omoniyi24 <omoniyi24@gmail.com>
2019-02-20 16:11:52 +0100

    First Commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require __DIR__ . '/vendor/autoload.php';
 Dompdf\Autoloader::register();
 // reference the Dompdf namespace
@@ -37,29 +37,65 @@ class PDFHelper {
     private $worksheet;
     private $spreadsheet;
 
-    public function getWorksheet($fileName){
+    public function getWorksheet($fileName, $download=false){
         /**  Create an Instance of our Read Filter  **/
         $filterSubset = new MyReadFilter($this->minPageNum, $this->maxPageNum,range('A','Y'));
 
         $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
         // $reader->setLoadSheetsOnly("Sheet1");
-        $reader->setReadFilter($filterSubset);
+        if ($download == false){
+            $reader->setReadFilter($filterSubset);
+        }
+        $this->fileName = $fileName;
         $this->spreadsheet = $reader->load($fileName);
         $this->worksheet = $this->spreadsheet->getSheet(0)->toArray(null, true, true, true);
 
         return $this->worksheet;
     }
 
-    public function getHTML(){
+    public function getTable($download=false){
         //html table with the excel file data
+        $this->getWorksheet($this->fileName, $download);
         $html_tb ='<font size="1" face="Courier New" >';
-        $html_tb .='<table border="1"><tr><th>'. implode('</th><th>', $this->worksheet[1]) .'</th></tr>';
+        $html_tb .='<table class="table" border="1"><tr><th>'. implode('</th><th>', $this->worksheet[1]) .'</th></tr>';
         $nr = count($this->worksheet); //number of rows
         for($i=2; $i<=$nr; $i++){
-          $html_tb .='<tr><td>'. implode('</td><td>', $this->worksheet[$i]) .'</td></tr>';
+            $html_tb .='<tr><td>'. implode('</td><td>', $this->worksheet[$i]) .'</td></tr>';
         }
         $html_tb .='</table></font>';
+
         return $html_tb;
+    }
+
+    public function getHtml(){
+        // added you custome html here to render the table
+        // it will also have the button and pagination
+        $html = '
+        //combo box to show a number of records per npage
+        <div>
+        <form action="index.php" method="post">
+        <label for="show_entries"> Show Entries : </label>
+        <select id="show_entries" name="show_entries" >
+           <option value="1">10</option>
+           <option value="2">20</option>
+           <option value="3">50</option>
+           <option value="4">100</option>
+        </select>
+
+        <input type="submit" name="search" value="Search"/>
+        </form>';
+
+        $html .= $this->getTable();
+        $html .=    '<form >
+                <a href="?file=csv">
+                <button type="button" onclick="downloadCSV()"> Download CSV </button>
+                </a>
+                <a href="?file=pdf">
+                <button type="button" onclick="downloadPDF()"> Download PDF </button>
+                </a>
+                
+            </form></div>';
+        return $html;
     }
 
     function getCSV(){
@@ -68,6 +104,7 @@ class PDFHelper {
         header('Content-Disposition: attachment;filename="myfile.csv"');
         header('Cache-Control: max-age=0');
 
+        $this->getWorksheet($this.$this->fileName, true);
         $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($this->spreadsheet);
         $writer->save('php://output');
     }
@@ -75,7 +112,7 @@ class PDFHelper {
     function getPDF(){
         // instantiate and use the dompdf class
         $dompdf = new Dompdf();
-        $dompdf->loadHtml($this->getHTML());
+        $dompdf->loadHtml($this->getTable(true));
 
         // (Optional) Setup the paper size and orientation
         $dompdf->setPaper('A2', 'landscape');
@@ -93,4 +130,14 @@ $fileName = __DIR__ ."/GooglePlaySept2018.xlsx";
 
 $pdfHelper = new PDFHelper();
 $pdfHelper->getWorksheet($fileName);
-echo $pdfHelper->getHTML();
+
+if (isset($_GET['file'])){
+    if ($_GET['file'] == 'pdf') {
+        echo $pdfHelper->getPDF();
+    }elseif ($_GET['file'] == 'csv'){
+        echo $pdfHelper->getCSV();
+    }
+}
+//echo $_SERVER['minPage'];
+//echo $_SERVER['maxPage'];
+echo $pdfHelper->getHtml();
